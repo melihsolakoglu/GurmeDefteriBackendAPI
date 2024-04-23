ae12f052b48e4016ecadef6f27d8fd423b116202
Said Saritemur <AliSaidSaritemur@gmail.com>
2024-04-23 22:05:54 +0300

    Yemek ekleme aktif hale geldi

---
+++ GurmeDefteriBackEndAPI/Models/ViewModel/FoodTemp.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace GurmeDefteriWebUI.Models.ViewModel
+{
+    public class FoodTemp
+    {
+        [StringLength(70, MinimumLength = 2, ErrorMessage = "İsim en az 2, en fazla 70 karakter olmalıdır.")]
+        [Required(ErrorMessage = "İsim alanı girilmelidir.\n")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Ülke alanı girilmelidir.\n")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Resim alanı girilmelidir.\n")]
+        public IFormFile Image { get; set; }
+    }
+}
